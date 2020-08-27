391b68f6412ad8fd69c75e359d2acc5565012c47
xieyj17 <yijun.xie@uwaterloo.ca>
2020-08-27 17:46:40 -0400

    update solution

--- Julia/P1.jl
+++ Julia/P1.jl
@@ -1,7 +1,11 @@
-ans = 0
-for i in 1:999
-    if (i %3 == 0) || (i %5 == 0)
-        global ans += i
+function myfun1(N)
+    ans = 0
+    for i in 1:2:(N-1)
+        if (i %3 == 0) || (i %5 == 0)
+            ans += i
+        end
     end
+    return ans
 end
-ans
+
+myfun1(1000)
---
+++ Julia/P4.jl
@@ -0,0 +1,29 @@
+function is_palindrome(n)
+    s = string(n)
+    l = length(s)
+    p = Int(floor(l / 2))
+    ans = true
+    for i in 1:p
+        if s[i] != s[l-i+1]
+            ans = false
+            break
+        end
+    end
+    return ans
+end
+
+function myfun4(ub, lb)
+    res = []
+    for i in ub:-1:lb
+        for j in ub:-1:i
+            if is_palindrome(i*j)
+                append!(res, i*j)
+                #return i*j
+                #break
+            end
+        end
+    end
+    return maximum(res)
+end
+
+myfun4(999,100)
